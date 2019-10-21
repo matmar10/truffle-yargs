0d6c9be9ccfcb9a48b06e076aad85f9a1cd84d2f
Matthew J. Martin <matthew.mar10@gmail.com>
2019-10-21 09:48:45 +0800

    add WIP (Broken) version using truffle config

--- example.js
+++ example.js
@@ -1,24 +1,21 @@
 'use strict';
 
-// const path = require('path');
+const path = require('path');
 const yargs = require('yargs');
 const truffleYargs = require('./')(yargs);
-const Web3 = require('web3');
 
-const provider = new Web3.providers.HttpProvider('http://localhost:8545');
+// const provider = new Web3.providers.HttpProvider('http://localhost:8545');
 
-truffleYargs('./contracts/*.json', {
-  contracts: {
-    Token: {
-
-    }
+truffleYargs(path.join(__dirname, '/../smartsukuk-dual-mudaraba/build/contracts/*.json'), {
+  truffle: {
+    workingDirectory: path.join(__dirname, '/../smartsukuk-dual-mudaraba'),
   },
   builder: {
     owner: {
       alias: 'address',
     },
   },
-  provider,
+  // provider,
   // truffleConfig: path.join(__dirname, '/../smartsukuk-dual-mudaraba/truffle.js'),
 })
   .demandCommand(1, 'Specify at least one command to run.')
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const AbiYargsAdapter = require('./lib/abi-yargs-adapter');
+const AbiYargsAdapter = require('./lib/yargs-adapter');
 const abiReader = require('./lib/abi-reader');
 
 const yargsBinderFactory = function (yargs) {
-  const factoryFunction = function (globPaths, options) {
+  const factoryFunction = function (globPathsOrMap, options) {
     const adapter = new AbiYargsAdapter(yargs, options);
     factoryFunction.abiYargsAdapter = adapter;
-    const contractAbiDefinitions = abiReader(globPaths, options);
+    const contractAbiDefinitions = abiReader(globPathsOrMap, options);
     return adapter.buildYargsFromArtifactMap(contractAbiDefinitions, options);
   };
   return factoryFunction;
