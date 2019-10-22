98f6919747237fe242ab905f037ae8653cc989ab
Matthew J. Martin <matthew.mar10@gmail.com>
2019-10-22 12:09:06 +0800

    fix options and fallback for dir and contracts

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 'use strict';
 
+const merge = require('deepmerge');
+const path = require('path');
 const AbiYargsAdapter = require('./lib/yargs-adapter');
 const abiReader = require('./lib/abi-reader');
 
+const defaultWorkingDirectory = path.join(__dirname, '/../../../');
+
+const defaults = {
+  workingDirectory: defaultWorkingDirectory,
+  contracts: path.join(defaultWorkingDirectory, 'build/contracts/*.json'),
+};
+
 const yargsBinderFactory = function (yargs) {
   const factoryFunction = function (options) {
-    const adapter = new AbiYargsAdapter(yargs, options);
+    const opts = merge(defaults, options);
+    const adapter = new AbiYargsAdapter(yargs, opts);
     factoryFunction.abiYargsAdapter = adapter;
-    const contractDefinitions = abiReader(options);
-    return adapter.buildYargsFromArtifactMap(contractDefinitions, options);
+    const contractDefinitions = abiReader(opts);
+    return adapter.buildYargsFromArtifactMap(contractDefinitions, opts);
   };
   return factoryFunction;
 };
